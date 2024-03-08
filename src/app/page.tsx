d083921d47d1ce7c608f123145ed60e29661de59
Xaldersoul <benjamin.g.guerra@hotmail.com>
2024-03-08 14:13:04 -0300

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Section />
       <BackgroundImageComponent image={BackgroundImage} />
       <ContentBlock
-        Icon={<GiPartyPopper className="text-7xl sm:text-8xl text-silver-50" />}
+        Icon={<GiPartyPopper className="text-silver-50" />}
         text="¡Te espero el Viernes 1 de Marzo de 2024 para festejar este gran momento de mi vida! Quinta Norte, Río Cuarto, Córdoba. Clickeá en el botón de abajo y encontrá las indicaciones para llegar, confirmar asistencia antes del 26/2!"
         title="FIESTA"
         btnText="CÓMO LLEGAR"
@@ -30,7 +30,7 @@ export default function Home() {
         url="https://docs.google.com/forms/d/e/1FAIpQLScGV3qXVLO5vdpg16u5OEG63L56eTsXfI4aQDYWRDsQmv4aCQ/viewform"
       />
       <ContentBlock
-        Icon={<FaMusic className="text-7xl sm:text-8xl text-silver-50" />}
+        Icon={<FaMusic className="text-silver-50" />}
         text="¡Ayudame sugiriendo las canciones que pensás que no pueden faltar en la fiesta!"
         title="¡MUESTRAME TUS GUSTOS!"
         btnText="SUGERIR CANCIÓN"
@@ -38,14 +38,12 @@ export default function Home() {
         url="https://docs.google.com/forms/d/e/1FAIpQLSeZUBN7JFagJ6Mn30gVakyxLbFTKkLk7Y4zmz1jGkJGE0yQig/viewform"
       />
       <ContentBlock
-        Icon={<GiLargeDress className="text-7xl sm:text-8xl text-silver-50" />}
+        Icon={<GiLargeDress className="text-silver-50" />}
         title="COLORES RESERVADOS"
         text="Informar que los colores Rojo, Azul y Lila estan reservados"
       />
       <ContentBlock
-        Icon={
-          <FaGlassCheers className="text-7xl sm:text-8xl text-nightblue-50" />
-        }
+        Icon={<FaGlassCheers className="text-nightblue-50" />}
         title="CENA Y BRINDIS"
         text="¡La cena tendra una costo de 14.000 (21:00 - 04:00) y el brindis 7.000 pesos (00:00 - 04:00) respectivamente!"
         alt
